bd4e31b659b774f23e9f82aa426f320cdf4906ec
alessandrobisiani <bisi90@gmail.com>
2019-04-04 23:09:32 -0400

    style: Adjust from pycodestyle output

--- database_setup.py
+++ database_setup.py
@@ -10,16 +10,19 @@ from sqlalchemy.ext.declarative import declarative_base
 # sqlalchemy entry point
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'users'
 
     email = Column(VARCHAR(350), primary_key=True)
     name = Column(String(250), nullable=False)
 
+
 class Category(Base):
     __tablename__ = 'categories'
 
     name = Column(String(100), primary_key=True)
+
 
 class Note(Base):
     __tablename__ = 'notes'
@@ -30,10 +33,11 @@ class Note(Base):
     title = Column(String(100), nullable=False)
     body = Column(BLOB())
 
-    category = relationship(Category)#back_populates='notes')
-    user = relationship(User)#back_populates='notes')
+    category = relationship(Category)  # back_populates='notes')
+    user = relationship(User)  # back_populates='notes')
+
 
 # create a database in notes.db and return a connection to it
-engine = create_engine('sqlite:///notes.db')
+engine = create_engine('sqlite:///alebisiani.db')
 # Instantiate the classes herein as tables.
 Base.metadata.create_all(engine)
