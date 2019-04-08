4648aef511568c7c35fd7e039883b97bee8bc1a1
alessandrobisiani <bisi90@gmail.com>
2019-04-08 16:52:28 -0400

    feat: Add html macros and basic styles for page layout
    Add part of google signin functionality for testing. To follow:
    * oauth functionality
    * db starting data
    * category display
    * etc.

--- notes/notes.py
+++ notes/notes.py
@@ -1,10 +1,21 @@
 #! /usr/bin/env python3.7
 from flask import Flask, render_template, request, flash, redirect, url_for, \
-        jsonify
+        jsonify, make_response
+
+from flask import session as login_session
+
+import httplib2
+import random, string, json
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 # from sqlalchemy.exc import DBAPIError
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+
 from database_setup import Base, Note, Category, User
+# import requests
 
 app = Flask(__name__)
 
@@ -20,7 +31,12 @@ session = DBSession()
 
 @app.route('/login/')
 def showLogin():
-    return 'Login page'
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
+    # state = hashlib.sha256(os.urandom(1024)).hexdigest()
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
 
 
 @app.route('/gconnect', methods=['POST'])
@@ -72,4 +88,5 @@ def deleteNote(category_name, id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'liseubpeu4no;3a4nga80[32443ifud5y4tu325zexrctouio78om;8n'
     app.run(host='0.0.0.0', port=5000, debug=True)
